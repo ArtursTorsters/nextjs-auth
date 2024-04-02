462a3c69f0f9a33673c6fa6c8122a4aa9fb1285f
Arturs Torsters <arrturs.torsters@gmail.com>
2024-04-02 12:16:01 +0300

    DB connect and routes

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+// connect to mongo cloud DB 
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection
+        connection.on('connected', () => {
+               console.log('connected') 
+
+        })
+    } catch (error) {
+        console.error('error')
+    }{}
+}
